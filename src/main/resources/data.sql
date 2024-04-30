750ee696f5b735ec93765b02022ff251d368a98a
渡邊丈洋 <take-watanabe@autobacs.com>
2024-04-30 10:50:32 +0900

    resultMap用「テーブル定義」「初期値」の更新

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,15 @@
+-- publishersテーブルへのデータ登録
+INSERT INTO publishers (name) values ('技術評論社');
+INSERT INTO publishers (name) values ('Technology評論社');
+
 -- booksテーブルへのデータ登録
-INSERT INTO books (title, author) VALUES ('新人研修あるある', '山田太郎');
-INSERT INTO books (title, author) VALUES ('こんなDBエンジニアは嫌だ', '佐藤花子');
-INSERT INTO books (title, author) VALUES ('本当にあったIT業界トラブル', '鈴木一郎');
+INSERT INTO books (title, author, publisher_id) VALUES ('新人研修あるある', '山田太郎', 1);
+INSERT INTO books (title, author, publisher_id) VALUES ('こんなDBエンジニアは嫌だ', '佐藤花子', 2);
+INSERT INTO books (title, author, publisher_id) VALUES ('本当にあったIT業界トラブル', '鈴木一郎', 1);
+
+-- reviewsテーブルへのデータ登録
+INSERT INTO reviews (book_id, review_text) VALUES (1, '非常に役立つ本でした。');
+INSERT INTO reviews (book_id, review_text) VALUES (2, 'こんなDBエンジニアがいることに驚愕しました。');
+INSERT INTO reviews (book_id, review_text) VALUES (2, '自分の現場には来てほしくないです。');
+INSERT INTO reviews (book_id, review_text) VALUES (2, '同じ様な方が、私の現場にいます。');
+INSERT INTO reviews (book_id, review_text) VALUES (3, '業界の闇を知るにはいい本です。');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,25 @@
--- booksテーブルの作成
-CREATE TABLE IF NOT EXISTS books (
-    -- 主キーとしてのID, 自動インクリメントされる
+-- 出版社の情報を格納するテーブル
+DROP TABLE IF EXISTS publishers;
+CREATE TABLE IF NOT EXISTS publishers (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    -- 書籍のタイトル、NULLを許容しない
-    title VARCHAR(255) NOT NULL,
-    -- 書籍の著者名、NULLを許容しない
-    author VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL
+);
+
+-- 書籍の情報を格納するテーブル
+DROP TABLE IF EXISTS books;
+CREATE TABLE IF NOT EXISTS books (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- 主キーとしてのID, 自動インクリメントされる
+    title VARCHAR(255) NOT NULL, -- 書籍のタイトル、NULLを許容しない
+    author VARCHAR(255) NOT NULL, -- 書籍の著者名、NULLを許容しない
+    publisher_id INT, -- 出版社ID。出版社テーブルと関連づける
+    FOREIGN KEY (publisher_id) REFERENCES publishers(id) -- 出版社テーブルへの外部キー制約
+);
+
+-- 書籍に対するレビューを格納するテーブル
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE IF NOT EXISTS reviews (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    book_id INT NOT NULL,
+    review_text TEXT,
+    FOREIGN KEY (book_id) REFERENCES books(id)
 );
